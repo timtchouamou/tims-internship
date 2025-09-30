c1d5893a1a63eceb375138852398fec78091db3b
timtchouamou <tim.tchouamou@gmail.com>
2025-09-29 20:06:16 -0400

    HotCollections pratice

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,43 +1,135 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+
+import axios from "axios";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const HotCollections = () => {
+  const [card, setCart] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function FectHotCollections() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`
+    );
+    console.log(data);
+    setCart(data);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }
+
+  useEffect(() => {
+    FectHotCollections();
+  }, []);
+
   return (
+
     <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
+          <div className="col-lg-12 text-center">
+            <h2>Hot Collections</h2>
+            <div className="small-border bg-color-2"></div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
         </div>
+
+        {loading ? (
+          <div className="row">
+            <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+             loop={true}                     // 👈 makes it 360° continuous
+            navigation
+            pagination={{ clickable: true }}
+            breakpoints={{
+              1024: { slidesPerView: 4 },
+              768: { slidesPerView: 2 },
+              0: { slidesPerView: 1 },
+            }}
+          >
+            {[...Array(4)].map((_, i) => (
+               <SwiperSlide key={i}>
+                <div className="nft_coll"
+                style={{ backgroundColor: "#d3d3deff" }}>
+                  <div className="nft_wrap">
+
+                    <div className="skeleton"/>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <div
+                      className="skeleton"
+                      style={{ borderRadius: "50%", width: 50, height: 50 }}
+                      
+                    />
+                  </div>
+                  <div className="nft_coll_info">
+                    <div className="skeleton-text" style={{ width: "80%" }} />
+                    <div className="skeleton-text" style={{ width: "50%" }} />
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+            </Swiper>
+          </div>
+
+        ) : (
+
+          <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={20}
+            slidesPerView={4}
+             loop={true}                     // 👈 makes it 360° continuous
+            navigation
+            pagination={{ clickable: true }}
+            breakpoints={{
+              1024: { slidesPerView: 4 },
+              768: { slidesPerView: 2 },
+              0: { slidesPerView: 1 },
+            }}
+          >
+            {card.slice(0, 8).map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to="/item-details">
+                      <img
+                        src={item.nftImage}
+                        className="lazy img-fluid"
+                        alt={item.title}
+                      />
+                    </Link>
+                  </div>
+
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img
+                        className="lazy pp-coll"
+                        src={item.authorImage}
+                        alt=""
+                      />
+                    </Link>
+                    <i className="fa fa-check" />
+                  </div>
+
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <span>ERC-{item.code}</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </section>
   );
