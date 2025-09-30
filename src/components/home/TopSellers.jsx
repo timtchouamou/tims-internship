be6e9e9d9df6d3dfcd1ee1f3d6d3960eeabaf4cc
timtchouamou <tim.tchouamou@gmail.com>
2025-09-30 04:02:05 -0400

    Top Seller Pratice

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+
+import "./TopSellers.css"
 
 const TopSellers = () => {
+  const [author, setAuthor] = useState([]);
+  const [loading, setLoading] = useState(true); // skeleton state
+
+  async function FecthTopSellers() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`
+    );
+    console.log(data);
+    setAuthor(data);
+
+    setTimeout(() => {
+      setLoading(false) 
+    }, 4000);
+
+  }
+
+  useEffect(() => {
+    FecthTopSellers();
+  }, []);
+
+  // skeleton placeholder (show while loading)
+  const skeletons = Array(12).fill(0);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +40,61 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+
+              {loading 
+               ? skeletons.map((_, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <div
+                          className="skeleton skeleton-img"
+                          style={{
+                            width: "50px",
+                            height: "50px",
+                            borderRadius: "50%",
+                          }}
+                        ></div>
+                      </div>
+                      <div className="author_list_info">
+                        <div
+                          className="skeleton skeleton-text"
+                          style={{ width: "100px", height: "15px" }}
+                        ></div>
+                        <div
+                          className="skeleton skeleton-text"
+                          style={{
+                            width: "60px",
+                            height: "12px",
+                            marginTop: "5px",
+                          }}
+                        ></div>
+                      </div>
+                    </li>
+                  )) 
+             
+              
+              :
+              
+               author.map((item, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
                     <Link to="/author">
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={item.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">{item.authorName}</Link>
+                    <span>{item.price} ETH</span>
                   </div>
                 </li>
-              ))}
+              ))
+
+              }
+
             </ol>
           </div>
         </div>
